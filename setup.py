febdd5ce08247c099bab9b7f6eb5a13612973053
Stanislav Khrapov <stanislav.khrapov@dbschenker.com>
2020-04-05 11:01:05 +0200

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name='vix',
     version='',
@@ -12,7 +9,7 @@ setup(
     author='Stanislav Khrapov',
     author_email='khrapovs@gmail.com',
     description='Compute VIX and its derivatives',
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     python_requires='>=3.6',
     install_requires=open('requirements.txt').read()
